2835c44046623cf4d76588717e5cb26de2283f4f
kelvinclever <kkelvinnzioka@gmail.com>
2023-07-06 07:10:46 +0300

    fullstack todo

---
+++ client/src/context/userContext/Context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useReducer } from "react";
+import Reducer from "./Reducer.jsx";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+};
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+  return (
+    <Context.Provider value={{ user: state.user, dispatch }}>
+      {children}
+    </Context.Provider>
+  );
+};
